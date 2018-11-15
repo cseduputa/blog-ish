19de457efe5449b9696744b52774567f06fb0a4a
cseduputa <43936152+cseduputa@users.noreply.github.com>
2018-11-15 10:31:20 -0500

    initial commit

---
+++ js/blog-ish.js
@@ -0,0 +1,11 @@
+function redirectToLogin() {
+    window.location.href = "sites/blog-ish-login.html";
+}
+
+function redirectToSignUp() {
+     window.location.href = "sites/blog-ish-sign-up.html";
+}
+
+function myFunction() {
+    console.log("button works");
+}
